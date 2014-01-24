0dc0bad46bbf0016f7a6b8d05352302839de22b7
Kriss <DriverInput@gmail.com>
2014-01-24 21:33:06 +0100

    object update
    shooting angles

--- GlobalGameJam/GlobalGameJam/GlobalGameJam/objects.cs
+++ GlobalGameJam/GlobalGameJam/GlobalGameJam/objects.cs
@@ -22,6 +22,14 @@ namespace GlobalGameJam
         public int imy;
         public int hp;
 
+        public float speed;
+        public float angle;
+        public float angle2;
+        public float scale_x;
+        public float scale_y;
+        public float veclocity_x;
+        public float veclocity_y;
+
         public bool destroy;
 
         public void setCoords(float x2, float y2)
@@ -43,5 +51,24 @@ namespace GlobalGameJam
         {
             spritebatch.Draw(spritesheet, new Vector2(x, y), new Rectangle(imx, imy, width, height), Color.White);
         }
+
+        public void math(float speed2)
+        {
+            angle2 = (angle * (float)Math.PI / 180);
+            speed = speed2;
+            scale_x = (float)Math.Cos(angle2);
+            scale_y = (float)Math.Sin(angle2);
+            veclocity_x = (speed * scale_x);
+            veclocity_y = (speed * scale_y);
+        }
+        public void mathAim(float speed2, float x2, float y2)
+        {
+            angle = (float)Math.Atan2(y2 - y, x2 - x);
+            speed = speed2;
+            veclocity_x = (speed * (float)Math.Cos(angle));
+            veclocity_y = (speed * (float)Math.Sin(angle));
+        }
+
+        
     }
 }
