4f5d41ed89586fa4afbbcb542c2d53c67be656cd
indietom <tom.leo@hotmail.com>
2014-01-25 14:05:58 +0100

    dasdasdasdasdasdasdasd

--- GlobalGameJam/GlobalGameJam/GlobalGameJam/enemy.cs
+++ GlobalGameJam/GlobalGameJam/GlobalGameJam/enemy.cs
@@ -7,5 +7,17 @@ namespace GlobalGameJam
 {
     class enemy:objects
     {
+        public int type;
+
+        public enemy(float x2, float y2, int type2)
+        {
+            setCoords(x2, y2);
+            setSize(24, 24);
+            type = type2;
+            switch (type)
+            {
+
+            }
+        }
     }
 }
