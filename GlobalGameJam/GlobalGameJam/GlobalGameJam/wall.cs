f6171d35f560d1a01c5906b5dd0ea413a7b1c64b
Kriss <DriverInput@gmail.com>
2014-01-25 16:47:05 +0100

    majs
    mackor

---
+++ GlobalGameJam/GlobalGameJam/GlobalGameJam/wall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GlobalGameJam
+{
+    class wall : objects
+    {
+        public wall()
+        {
+            setSpriteCoords(1,451);
+            setSize(16, 16);
+        }
+    }
+}
